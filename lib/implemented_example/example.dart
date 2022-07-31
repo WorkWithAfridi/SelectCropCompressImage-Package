c4a6636136047c2f56e9fbeebf3fd20b06c62606
Kyoto <khondakarafridi35@gmail.com>
2022-07-31 18:21:42 +0600

    Commented out example codes

--- lib/implemented_example/example.dart
+++ lib/implemented_example/example.dart
@@ -2,30 +2,30 @@ import 'dart:js';
 import 'dart:typed_data';
 import 'package:selectcropcompressimage/selectcropcompressimage.dart';
 
-Future getImageFromGallery() async {
-  SelectCropCompressImage selectCropCompressImage = SelectCropCompressImage();
-  Uint8List? selectedCroppedAndCompressImage =
-      await selectCropCompressImage.selectCropCompressImageFromCamera(
-    compressionAmount: 30,
-    context: context,
-  );
-  if (selectedCroppedAndCompressImage != null) {
-    //Do something with the image
-  } else {
-    //Discard...
-  }
-}
+// Future getImageFromGallery() async {
+//   SelectCropCompressImage selectCropCompressImage = SelectCropCompressImage();
+//   Uint8List? selectedCroppedAndCompressImage =
+//       await selectCropCompressImage.selectCropCompressImageFromCamera(
+//     compressionAmount: 30,
+//     context: context,
+//   );
+//   if (selectedCroppedAndCompressImage != null) {
+//     //Do something with the image
+//   } else {
+//     //Discard...
+//   }
+// }
 
-Future getImageFromCamera() async {
-  SelectCropCompressImage selectCropCompressImage = SelectCropCompressImage();
-  Uint8List? selectedCroppedAndCompressImage =
-      await selectCropCompressImage.selectCropCompressImageFromCamera(
-    compressionAmount: 30,
-    context: context,
-  );
-  if (selectedCroppedAndCompressImage != null) {
-    //Do something with the image
-  } else {
-    //Discard...
-  }
-}
+// Future getImageFromCamera() async {
+//   SelectCropCompressImage selectCropCompressImage = SelectCropCompressImage();
+//   Uint8List? selectedCroppedAndCompressImage =
+//       await selectCropCompressImage.selectCropCompressImageFromCamera(
+//     compressionAmount: 30,
+//     context: context,
+//   );
+//   if (selectedCroppedAndCompressImage != null) {
+//     //Do something with the image
+//   } else {
+//     //Discard...
+//   }
+// }
